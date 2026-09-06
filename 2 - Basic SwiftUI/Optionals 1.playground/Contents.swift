532a94c16ff2e29f4fd45118f4999fa79831a210
Joseph Swensen <joeyswensen@icloud.com>
2026-09-05 20:35:41 -0600

    adding Optionals 1 lab

--- 2 - Basic SwiftUI/Optionals 1.playground/Contents.swift
+++ 2 - Basic SwiftUI/Optionals 1.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+var temperature: Double? //nil
+var testScore: Int? // nil becasue they might have missed the test and their score was not put in for them
+var phoneNumber: String // you should alwasy have a phone number so people can contact you
+var middleName: String? //nil some people dont have middle names
+var discountPercentage: Double = 0 // there might noit be a discount
+var allergies: String? // the person might no have alergies
+var flightArrivalTime: String // you always need a arrival time becasue how will people know when they get to their destination
+var hairColor: String? // nil we dont need to know your hair color
+var socialMediaHandle: String? // nil people might not have one
+var meetingNotes: String? //nil people might not take meeting notes or want to share them
